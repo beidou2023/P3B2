bdf5d7d71030b1bf03e4170328cf65596e724c63
Cruz Carlos <ccruzworks@gmail.com>
2024-08-31 18:43:41 -0400

    Plantilla xml de login

--- app/src/main/java/com/example/practica3b/firstAct.java
+++ app/src/main/java/com/example/practica3b/firstAct.java
@@ -1,5 +1,6 @@
 package com.example.practica3b;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login extends AppCompatActivity {
+public class firstAct extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_first_act);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Personal personal=new Personal();
+        MySingleton.setPersonal(personal);
+
+        Intent intent = new Intent(getApplicationContext(), login.class);
+        startActivity(intent);
     }
 }
